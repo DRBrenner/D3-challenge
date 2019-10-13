18a28e7b5fa92f76c841cb910f0d1ad17c4e464a
Dawn <brenner95670@gmail.com>
2019-10-12 23:24:20 -0700

    Finalized homework and completed ReadMe

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -5,7 +5,7 @@ var svgHeight = innerHeight;
 // Define the chart's margins as an object
 var margin = {
     top: 60,
-    right: 60,
+    right: 260,
     bottom: 60,
     left: 60
 };
@@ -39,121 +39,82 @@ d3.csv("data.csv").then(function (healthData) {
         console.log(`State: ${data.abbr}, Obesity: ${data.obesity}, Poverty: ${data.poverty}`)
         obesity.push(+data.obesity)
         poverty.push(+data.poverty)
-
-        console.log(obesity)
     });
 
 
-    // // Set the domain for the xLinearScale function (min and max of obesity data)
+    // // Set the domain for the xLinearScale function (min and max of obesity data +/-3)
     var xLinearScale = d3.scaleLinear()
-        // .range([0, chartWidth])
-        // .domain([0, d3.max(obesity)]);
-        .range([0, 100])
-        .domain([0, 100]);
-
-    console.log("obesity", d3.extent(obesity))
+        .range([0, chartWidth])
+        .domain([d3.extent(obesity)[0] - 3, d3.extent(obesity)[1] + 3]);
 
     // // Configure a linear scale with a range between the chartHeight and 0
-
-
-    // // Set the domain for the yLinearScale function (0 to max of poverty data)
+    // // Set the domain for the yLinearScale function (min and max of poverty data +/-3)
     var yLinearScale = d3.scaleLinear()
-        // .domain([0, d3.max(poverty)])
-        // .range([chartHeight, 0]);
-        .domain([0, 100])
-        .range([100, 0]);
-
-    console.log("poverty", d3.max(poverty))
-
-    console.log("xScale", xLinearScale);
-    console.log("yScale", yLinearScale);
+        .domain([d3.extent(poverty)[0] - 3, d3.extent(poverty)[1] + 3])
+        .range([chartHeight, 0]);
 
     // Create two new functions passing the scales in as arguments
     // These will be used to create the chart's axes
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
-    console.log(bottomAxis)
 
     // Draw circles to represent each state
     svg.selectAll("circle")
         .data(healthData) // assign csv data to correspond to each node
         .enter().append("circle")
-        .attr("cx", d=> xLinearScale(+d.obesity))
-        .attr("cy", d=> yLinearScale(+d.poverty))
-        .attr("r", 2.5);
+        .attr("transform", `translate(${margin.left}, ${margin.top})`)
+        .attr("cx", d => xLinearScale(+d.obesity))
+        .attr("cy", d => yLinearScale(+d.poverty))
+        .attr("r", 15)
+        .attr("fill", "blue")
+        .attr("stroke", "black")
+        .style("stroke-width", 2)
+        .attr("opacity", ".4");
 
-    // var drawLine = d3
-    // .line()
-    // .x(data => xTimeScale(data.date))
-    // .y(data => yLinearScale(data.miles));
+    chartGroup.append("g")
+        .classed("axis", true)
+        .call(leftAxis);
 
+    //Create y axis label
+    chartGroup.append("g")
+        .append("text")
+        .attr("class", "y label")
+        .attr("text-anchor", "end")
+        .attr("transform", "translate(200, 925)")
+        .attr("y", -40)
+        .attr("dy", ".75em")
+        .attr("transform", "rotate(-90)")
+        .text("Poverty");
 
+    //Create x axis label    
+    chartGroup.append("g")
+        .append("text")
+        .attr("fill", "black")
+        .attr("transform", "translate(530, 525)")
+        .text("Obesity")
+        ;
 
+    chartGroup.append("g")
+        .classed("axis", true)
+        .attr("transform", "translate(0, " + chartHeight + ")")
+        .call(bottomAxis);
 
-
-
-
-    // .enter().append("svg:circle")
-    // .attr("class", "little")
-    // // arbritrary position of x value corresponding to index of node
-    // .attr("cx", function (d, i) {
-    //     return (i + 1) * 50;
-    // })
-    // .attr("cy", 90)
-    // // radius = log of population
-    // .attr("r", function (d) {
-    //     return Math.pow(size(parseInt(d["population"])), 2);
-    // })
-    // // color = log of GDP
-    // .attr("fill", function (d) {
-    //     return color(parseInt(d["gdp"]));
-    // });
-    //     // Draw circles to represent each "node" or country
-    //     svg.selectAll("text")
-    //         .data(nodes)
-    //         .enter().append("svg:text")
-    //         .text(function (d) {
-    //             return d["name"];
-    //         })
-    //         .attr("text-anchor", "middle")
-    //         .attr("x", function (d, i) {
-    //             return (i + 1) * 150;
-    //         })
-    //         .attr("y", 90)
-    // });
-
+    // Appending a label to each data circle
+    chartGroup.append("text")
+        .style("text-anchor", "middle")
+        .style("font-size", "12px")
+        .selectAll("tspan")
+        .data(healthData)
+        .enter()
+        .append("tspan")
+        .attr("x", function (data) {
+            return xLinearScale(data.obesity - 0);
+        })
+        .attr("y", function (data) {
+            return yLinearScale(data.poverty - 0.2);
+        })
+        .text(function (data) {
+            return data.abbr
+        });
 })
-
-// Inside function need to loop through each row
-// create circle element from
-// x = poverty
-// y = obesity
-// c = smokes
-// circle label = state abbr
-
-
-//d3.csv("data.csv", function (healthData) {
-
-
-    // // Get min and max smokes to use in color function
-    // var smokes_min = d3.min(nodes, function(d){
-    // 	return d.smokes;
-    // });
-    // var smokes_max = d3.max(nodes, function(d){
-    //     return parseInt(d["smokes"]);
-    // });
-    // console.log(smokes_min);
-    // console.log(smokes_max);
-
-    // Size and Color will correspond to the log of GDP and Population
-//     var size = d3.scale.log();
-//     var color = d3.scale.log()
-//         .domain([gdp_min, gdp_max])
-//         .interpolate(d3.interpolateRgb)
-//         .range(["skyblue", "steelblue"]);
-
-//     
-//</script>
-//</body>
-//</html>
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# D3-challenge
+# D3 Homework - Data Journalism and D3
+
+![Newsroom](https://media.giphy.com/media/v2xIous7mnEYg/giphy.gif)
+
+## Purpose
+
+The purpose of this assignment was to show my understanding of D3 and Javascript.
+
+Using knowledge of HTML and JS, I created an SVG area. I used D3 to pull data from a .csv file.  I found the min and max of the chosen data for the x and y axis and scaled them appropriately.  I plotted a scattergram and labeled each node with the US state represented by the node.
+
+### Dataset provided
+
+.csv - U.S. Census Bureau and the Behavioral Risk Factor Surveillance System - 2014 ACS 
+
+### Tools used
+
++1. Git Hub
++2. Python server
++3. JavaScript
++4. D3
++5. HTML
